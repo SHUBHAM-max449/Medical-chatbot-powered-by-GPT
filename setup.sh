a796d1550b192c14a22c0cd80965706c2119178e
Shubham <chitaguppeshubham007@gmail.com>
2025-07-21 11:49:52 +0200

    folder structure added

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch research/__init.py
+touch src/helper.py
+touch src/prompt.py
+touch setup.py
+touch .env
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo "folder structure created successfully"
