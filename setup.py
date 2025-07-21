8d4b0d556ce779d16d097b8472d8606814346439
Shubham <chitaguppeshubham007@gmail.com>
2025-07-21 13:26:29 +0200

    packages installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+   name="medical_chatbot",
+   version="0.1.0",
+   author="Shubham Chitaguppe",
+   author_email="CHITAGUPPESHUBHAM007@GMAIL.COM",
+   packages=find_packages(),
+   install_requires=[]
+)
